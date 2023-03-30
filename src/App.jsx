bc63f359f257b76a6144043d1b2fc6b5b5eb3557
Chris Mittell <mittell@hotmail.co.uk>
2023-03-30 17:41:08 +0900

    feat: added Stars component

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,17 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<div className='relative z-0 bg-primary'>
-				<div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-					<Navbar />
-					<Hero />
-				</div>
-				<About />
-				<Experience />
-				<Tech />
-				<Work />
-				<Feedback />
 				<div className='relative z-0'>
+					<div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+						<Navbar />
+						<Hero />
+					</div>
+					<About />
+					<Experience />
+					<Tech />
+					<Work />
+
+					<Feedback />
 					<StarsCanvas />
 				</div>
 			</div>
