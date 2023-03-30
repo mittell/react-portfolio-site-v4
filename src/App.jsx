a2a332fc2ba8a795fd013ac9a353af86b0c77068
Chris Mittell <mittell@hotmail.co.uk>
2023-03-30 17:22:41 +0900

    chore: removed Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import {
 	About,
-	Contact,
 	Experience,
 	Feedback,
 	Hero,
@@ -25,7 +24,6 @@ const App = () => {
 				<Work />
 				<Feedback />
 				<div className='relative z-0'>
-					<Contact />
 					<StarsCanvas />
 				</div>
 			</div>
