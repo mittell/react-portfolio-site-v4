fb3e2f4a2f496e72c828bf26e65e90478b8c2ca1
Chris Mittell <mittell@hotmail.co.uk>
2023-03-30 13:10:55 +0900

    chore: corrected spelling

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,4 @@
-import {
-	EarthCanvas,
-	BallCanvas,
-	ComputersCanvas,
-	StarsCanvas,
-} from './canvas';
+import { EarthCanvas, BallCanvas, ComputerCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
@@ -24,6 +19,6 @@ export {
 	Contact,
 	EarthCanvas,
 	BallCanvas,
-	ComputersCanvas,
+	ComputerCanvas,
 	StarsCanvas,
 };
