421f753d50b8d762fd5e7c8f33744e4bebd0a14d
Chris Mittell <mittell@hotmail.co.uk>
2023-03-30 12:20:01 +0900

    feat: initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+	About,
+	Contact,
+	Experience,
+	Feedback,
+	Hero,
+	Navbar,
+	Tech,
+	Work,
+	StarsCanvas,
+} from './components';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<div className='relative z-0 bg-primary'>
+				<div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+					<Navbar />
+					<Hero />
+				</div>
+				<About />
+				<Experience />
+				<Tech />
+				<Work />
+				<Feedback />
+				<div className='relative z-0'>
+					<Contact />
+					<StarsCanvas />
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 };
 
 export default App;
